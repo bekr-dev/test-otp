c00c85af41bcae8f2ac18ee534f957824bb87a88
bekr-dev <125211353+bekr-dev@users.noreply.github.com>
2026-06-03 00:49:39 +0100

    Import express Router and update mailRouter export
    Added import statement for express Router and updated export.

--- Routers/mail.route.mjs
+++ Routers/mail.route.mjs
@@ -1,9 +1,10 @@
-
-
+import { Router } from "express";
+// استيراد الدالة مباشرة بين أقواس {}
 import { send } from "../Controllers/mail.controller.mjs";
 
 const mailRouter = Router();
 
+// استدعاء الدالة مباشرة دون استخدام كائن
 mailRouter.post('/send', send);
 
 export default mailRouter;
