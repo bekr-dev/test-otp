d4ac0b217677c03e80b10e590186da72770b98d9
bekr-dev <125211353+bekr-dev@users.noreply.github.com>
2026-06-08 11:46:27 +0100

    Update mail.mjs

--- Utils/mail.mjs
+++ Utils/mail.mjs
@@ -8,7 +8,7 @@ export const sendOTP = async (to,code, subject="Email Verification") => {
     const min = 100000; const max = 999999;
     //const otp = Math.floor(Math.random() * (max - min)) + min
     const mailOptions = {
-        from: '"MediConnect  " <test' + process.env.EMAIL + '>',
+        from: '"MediConnect" <' + process.env.EMAIL + '>',
         to: to,
         subject: subject,
         html: `
